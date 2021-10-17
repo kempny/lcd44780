39360bbca5fbbc65a4ca0725f09bd1bd4ecafa57
Bogusław Kempny <kempny@stanpol.com.pl>
2021-10-17 15:37:21 +0200

    Add files via upload

--- examples/demo.c
+++ examples/demo.c
@@ -17,17 +17,18 @@
 
 #define I2C_ADDR0   0x26 // I2C device address
 #define I2C_ADDR1   0x27 // I2C device address
+#define I2C_ADDR2   0x23 // I2C device address
 
 
 
 
-/*  zmienne  */
 char buftime[25];
 
 
 int free_disp;
 int display0;
 int display1;
+int display2;
 
 int n, m;
 time_t czas1;
@@ -44,6 +45,8 @@ display0=i2copen(1, I2C_ADDR0);
 lcdinit(display0, 4, 16); // setup LCD
 display1=i2copen(1, I2C_ADDR1);
 lcdinit(display1, 4, 20); // setup LCD
+display2=i2copen(1, I2C_ADDR2);
+lcdinit(display2, 2, 16); // setup LCD
 
 free_disp=0;
 
@@ -52,11 +55,13 @@ free_disp=0;
 lcdSet(display0,1, 1, 0, 0); 
 lcdSet(display1,1, 1, 0, 0); 
 sleep(1);
-lcdSet(display1,1, 0, 0, 1); 
 lcdSet(display0,1, 0, 0, 1); 
+lcdSet(display1,1, 0, 0, 1); 
+lcdSet(display2,1, 0, 0, 1); 
 
 lcdClr(display0);
 lcdClr(display1);
+lcdClr(display2);
 sleep(1);
 
 time (&czas1);            
@@ -67,13 +72,17 @@ lcdLine(display0,0,0,"P234567890");
 lcdLine(display0,10,1,"D23456789K");
 lcdLine(display0,10,2,"T23");
 
+lcdLine(display2,0,0,"P234567890");
+lcdLine(display2,0,1,"D23456789K");
+
 lcdLine(display1,0,0,"P234567890");
-//lcdLine(display1,0,1,"D234567890");
-//lcdLine(display1,0,2,"T234567890");
 lcdLine(display1,10,1,"D123456789K");
 lcdLine(display1,10,2,"T23");
 //lcdLine(display1,0,3,buftime);
 sleep(1);
+
+lcdClr(display1);
+lcdBlock(display1,5,2,"block01234567890123456789");
 
 lcdClr(display0);
 lcdLoc(display0, 0,0);
