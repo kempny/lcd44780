be22cf9b3bb68ba295999facb52bb6e785df8d75
Bogusław Kempny <kempny@stanpol.com.pl>
2021-10-19 14:03:09 +0200

    Add files via upload

--- examples/demo.c
+++ examples/demo.c
@@ -13,18 +13,19 @@
 #include <unistd.h>
 #include <linux/i2c-dev.h>
 #include <lcdi2c.h>
+#include <pthread.h>
 
 
+
+// 3 displays,   i2c addresses
 #define I2C_ADDR0   0x26 // I2C device address
 #define I2C_ADDR1   0x27 // I2C device address
 #define I2C_ADDR2   0x23 // I2C device address
 
 
-
 char buftime[25];
 
 
-int free_disp;
 int display0;
 int display1;
 int display2;
@@ -33,6 +34,23 @@ int n, m;
 time_t czas1;
 struct tm *loctime;
 
+void *disptime(void *ptr)
+{
+int fdlcd;
+int i;
+
+  fdlcd =  (int)ptr;
+
+  for(;;)
+   {
+    time (&czas1);
+    loctime = localtime (&czas1);
+    strftime (buftime, 25, "%Y-%m-%d  %H:%M:%S", loctime);
+    lcdLine(fdlcd,0,3,buftime);
+    Delay_mls(500);
+   }
+}
+
 
 /* S T A R T */
 void main (argc, argv)
@@ -40,54 +58,61 @@ int argc;
 char *argv[];
 
 {
-display0=i2copen(1, I2C_ADDR0);
-lcdinit(display0, 4, 16); // setup LCD
-display1=i2copen(1, I2C_ADDR1);
-lcdinit(display1, 4, 20); // setup LCD
-display2=i2copen(1, I2C_ADDR2);
-lcdinit(display2, 2, 16); // setup LCD
 
 
+// display 0, 4 lines, 16 columns
+  display0=i2copen(1, I2C_ADDR0);
+  lcdinit(display0, 4, 16); // setup LCD
+
+// display 1, 4 lines, 20 columns
+  display1=i2copen(1, I2C_ADDR1);
+  lcdinit(display1, 4, 20); // setup LCD
+
+// display 2, 2 lines, 16 columns
+  display2=i2copen(1, I2C_ADDR2);
+  lcdinit(display2, 2, 16); // setup LCD
 
 
-lcdSet(display0,1, 1, 0, 0); 
-lcdSet(display1,1, 1, 0, 0); 
-sleep(1);
-lcdSet(display0,1, 0, 0, 1); 
-lcdSet(display1,1, 0, 0, 1); 
-lcdSet(display2,1, 0, 0, 1); 
+// turn display and backlight on, cursor and blinking off
+  lcdSet(display0,1, 0, 0, 1); 
+  lcdSet(display1,1, 0, 0, 1); 
+  lcdSet(display2,1, 0, 0, 1); 
 
+//Clear displays
+  lcdClr(display0);
+  lcdClr(display1);
+  lcdClr(display2);
+
+// Create thread to display date and time on display 1
+
+  pthread_t thread_id;
+
+  pthread_create (&thread_id, NULL, disptime, (void *)display1);
+
+//Display animated text 
+lcdRun(display0, 1, 5, 0, "To be or not to be, that is the question     ");
+lcdRun(display1, 1, 5, 0, "A123456789B123456789                    ");
+lcdRun(display2, 1, 9, 1, "To be or not to be, that is the question     ");
+sleep(4);
+
+//stop displaying animated text on display 0
+lcdStop(display0);
+
+//Display line of text on display 1
+lcdLine(display1,0,1,"ABCDEFGHI");
+sleep(2);
+
+// Clear display 0
 lcdClr(display0);
-lcdClr(display1);
-lcdClr(display2);
-sleep(1);
+sleep(2);
 
-time (&czas1);            
-loctime = localtime (&czas1);
-strftime (buftime, 25, "%Y-%m-%d  %H:%M:%S", loctime);
 
-lcdLine(display0,0,0,"P234567890");
-lcdLine(display0,10,1,"D23456789K");
-lcdLine(display0,10,2,"T23");
-
-lcdLine(display2,0,0,"P234567890");
-lcdLine(display2,0,1,"D23456789K");
-
-lcdLine(display1,0,0,"P234567890");
-lcdLine(display1,10,1,"D123456789K");
-lcdLine(display1,10,2,"T23");
-sleep(1);
-
+//Display block of texy on display 0
+lcdBlock(display0,5,1,"block 0123456789 0123456789 01234567890");
+sleep(4);
 lcdClr(display0);
-lcdClr(display1);
-lcdClr(display2);
-lcdBlock(display0,5,2,"block01234567890123456789");
-lcdBlock(display1,5,2,"block01234567890123456789");
-lcdBlock(display2,5,1,"block01234567890123456789");
 
-sleep(3);
-lcdLine(display1,0,3,buftime);
-lcdClr(display0);
+//display single characters at  display 0
 lcdLoc(display0, 0,0);
 lcdChar(display0, 'A');
 lcdLoc(display0, 5,1);
@@ -97,7 +122,40 @@ lcdChar(display0, 'C');
 lcdLoc(display0, 15,3);
 lcdChar(display0, 'D');
 
-}                               // od main
+sleep(3);
+
+//Stop animation on display2
+lcdStop(display2);
+sleep(2);
+
+// Clear display2, display some text
+lcdClr(display2);
+lcdLine(display2,6,0,"Test");
+sleep(2);
+
+//Switch off display 2
+  lcdSet(display2,0, 0, 0, 1); 
+sleep(2);
+
+//Switch on display 2
+  lcdSet(display2,1, 0, 0, 1); 
+sleep(2);
+
+//Stop animation on display1
+lcdStop(display1);
+
+lcdClr(display0);
+lcdClr(display1);
+lcdClr(display2);
+lcdLine(display0,5,1,"The End");
+lcdLine(display1,6,1,"The End");
+lcdLine(display2,5,0,"The End");
+
+//  the time will be displayed by thread
+
+sleep(3);
+
+}                              
 
 
 
